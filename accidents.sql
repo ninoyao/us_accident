0daaa2a1416f1ddcc6370a1b7bbc32ca66b91bf4
Yuzhi(Nino) Yao <ninoyao@gmail.com>
2020-04-01 16:22:42 -0500

    sql

--- accidents.sql
+++ accidents.sql
@@ -7,8 +7,9 @@ USE accidents;
 #before loading database go preferences -> SQL Editor and change DBMS connection read time out (in seconds): 300
 # https://jeffreyeverhart.com/2017/11/04/mysql-workbench-error-code-2013-lost-connection-mysql-server-query/
 
-
+DROP TABLE IF EXISTS master_table;
 CREATE TABLE master_table(
+`X1` VARCHAR(50),
 `ID` VARCHAR(50) PRIMARY KEY,
 `Source` VARCHAR(20),
 `TMC` INT NULL , #SHOULD BE A NUMBER
@@ -17,22 +18,22 @@ CREATE TABLE master_table(
 `End_Time` DATETIME,
 `Start_Lat` DECIMAL(10,6) ,
 `Start_Lng` DECIMAL(10,6) ,
-`End_Lat` DECIMAL(10,6) NULL,
-`End_Lng` DECIMAL(10,6) NULL,
+`End_Lat` DECIMAL(10,6) DEFAULT NULL,
+`End_Lng` VARCHAR(50) DEFAULT NULL,
 `Distance(mi)` DECIMAL (8,2) ,
 `Description` BLOB,
 `Number` INT NULL , #SHOULD BE A NUMBER 
 `Street` VARCHAR(120),
-`Side` VARCHAR(2),
+`Side` VARCHAR(10),
 `City` VARCHAR(50),
-`County` VARCHAR(50),
+`County` VARCHAR(100),
 `State` VARCHAR(10),
 `Zipcode` VARCHAR(15),
-`Country` VARCHAR(2),
+`Country` VARCHAR(200),
 `Timezone` VARCHAR(20),
 `Airport_Code` VARCHAR(6),
 `Weather_Timestamp` DATETIME NULL,#SHOULD BE A DATETIME
-`Temperature(F)` DECIMAL(4,2) NULL, #SHOULD BE A NUMBER
+`Temperature(F)` DECIMAL(5,2) NULL, #SHOULD BE A NUMBER
 `Wind_Chill(F)` DECIMAL(4,2) NULL , #SHOULD BE A NUMBER
 `Humidity(%)` DECIMAL(5,2) NULL  , #SHOULD BE A NUMBER
 `Pressure(in)` DECIMAL(4,2) NULL  , #SHOULD BE A NUMBER
@@ -54,15 +55,17 @@ CREATE TABLE master_table(
 `Traffic_Calming` SET('True', 'False')  , #SHOULD BE A BOOL VALUE
 `Traffic_Signal` SET('True', 'False')  , #SHOULD BE A BOOL VALUE
 `Turning_Loop` SET('True', 'False')  , #SHOULD BE A BOOL VALUE
-`Sunrise_Sunset` VARCHAR(6),
-`Civil_Twilight` VARCHAR(6),
-`Nautical_Twilight` VARCHAR(6),
-`Astronomical_Twilight` VARCHAR(6)
+`Sunrise_Sunset` VARCHAR(100),
+`Civil_Twilight` VARCHAR(100),
+`Nautical_Twilight` VARCHAR(100),
+`Astronomical_Twilight` VARCHAR(100)
 );
 
+ALTER TABLE `accidents`.`master_table` 
+ENGINE = InnoDB ;
+#LOAD DATA LOCAL INFILE '/Users/linh/Documents/Vanderbilt/Spring2020/DMS_5420/Project2/US_Accidents_Dec19.csv'
 
-#LOAD DATA LOCAL INFILE 'C:\\Users\\ninoy\\Desktop\\VandyDS\\2020spring\\dbms\\US_Accidents_Dec19.csv'
-LOAD DATA LOCAL INFILE '/Users/linh/Documents/Vanderbilt/Spring2020/DMS_5420/Project2/US_Accidents_Dec19.csv'
+LOAD DATA INFILE 'C:\\Users\\ninoy\\Desktop\\VandyDS\\2020spring\\dbms\\us_accident\\accident_na.csv'
 INTO TABLE master_table
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n' 
